587480a7ca776dae91df665b7189911998e38429
Ruy Asan <ruyasan@gmail.com>
2014-09-04 15:43:25 -0700

    add our service scripts

--- lib/dsi_deploy/capistrano.rb
+++ lib/dsi_deploy/capistrano.rb
@@ -36,3 +36,46 @@ set :dsi_deploys, -> {
     DSI::Deploy.new(name, conf, self)
   end
 }
+
+
+namespace :web do
+  %w[start stop restart reload].each do |command|
+    desc "#{command} web-server"
+    task command do
+      on roles(:web) do
+        execute :sudo, :service, fetch(:application), command
+      end
+    end
+  end
+
+  after 'deploy:finished', 'web:start-or-reload' do
+    on roles(:web) do
+      if capture(:sudo, :service, fetch(:application), :status) =~ /\w+\/running/
+        execute :sudo, :service, fetch(:application), :reload
+      else
+        execute :sudo, :service, fetch(:application), :start
+      end
+    end
+  end
+end
+
+namespace :workers do
+  %w[start stop restart].each do |command|
+    desc "#{command} worker service"
+    task command do
+      on roles(:worker) do
+        execute :sudo, :service, "#{fetch(:application)}-workers", command
+      end
+    end
+  end
+
+  after 'deploy:finished', 'workers:start-or-restart' do
+    on roles(:worker) do
+      if test :sudo, :service, "#{fetch(:application)}-workers", :status
+        execute :sudo, :service, "#{fetch(:application)}-workers", :restart
+      else
+        execute :sudo, :service, "#{fetch(:application)}-workers", :start
+      end
+    end
+  end
+end
